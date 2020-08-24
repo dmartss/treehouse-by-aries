fdc32bd31764cf0396b9de433f07ef81d6b6638a
Dan <dan@ariesliving.com>
2020-08-24 10:46:51 -0400

    fix errors

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,15 @@
 import SectionHeader from 'components/section-header'
+import { NAME, TEXT } from 'lib/constants'
 import Container from 'components/container'
-import Page from 'components/page'
 import Image from 'components/image'
-import { NAME_SHORT, TEXT } from 'lib/constants'
+import Page from 'components/page'
 
 export default function About() {
   return (
     <Page title="About">
       <Container role="main" dotBackground aria-labelledby="about">
         <Container center padding>
-          <SectionHeader id="about" title={`About ${NAME_SHORT}`} description={TEXT[0]} />
+          <SectionHeader id="about" title={`About ${NAME[0]}`} description={TEXT[0]} />
           <Image
             shadow
             className="no-drag"
